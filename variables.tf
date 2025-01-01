e83c634eab388cdd7a426255d8fad5bdd5239b16
ptonini <pedro.tonini@gmail.com>
2024-12-31 21:16:48 -0300

    chore: first release

---
+++ variables.tf
@@ -0,0 +1,56 @@
+variable "name" {}
+
+variable "rg" {
+  type = object({
+    name     = string
+    location = string
+  })
+}
+
+variable "randomize_name" {
+  default  = true
+  nullable = false
+}
+
+variable "account_tier" {
+  default  = "Standard"
+  nullable = false
+}
+
+variable "account_replication_type" {
+  default  = "LRS"
+  nullable = false
+}
+
+variable "queue_encryption_key_type" {
+  default  = "Service"
+  nullable = false
+}
+
+variable "table_encryption_key_type" {
+  default  = "Service"
+  nullable = false
+}
+
+variable "infrastructure_encryption_enabled" {
+  default  = true
+  nullable = false
+}
+
+variable "containers" {
+  type = map(object({
+    access_type = string
+  }))
+  default  = {}
+  nullable = false
+}
+
+variable "shares" {
+  type = map(object({
+    quota            = number
+    access_tier      = optional(string, "Hot")
+    enabled_protocol = optional(string, "SMB")
+  }))
+  default  = {}
+  nullable = false
+}
